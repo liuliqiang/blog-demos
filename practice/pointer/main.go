e140adb6267655f6212123bf7fb4eacfad3ea39e
Liu Liqiang <liuliqiang@smartx.com>
2019-11-18 00:15:29 +0800

    [Kubernetes Tutorial] Add config map examples

--- practice/pointer/main.go
+++ practice/pointer/main.go
@@ -2,40 +2,29 @@ package main
 
 import "fmt"
 
+// created by: https://liqiang.io
 func main() {
 	a := []int{1, 2, 3}
-	slice(a)
-
-	b := [][]int{}
-	fmt.Printf("before: %p\n", &b)
-	twoSlice(b)
-	fmt.Printf("after: %p\n", &b)
-
-	c := [][]int{}
-	twoSlicePointer(&c)
-
-	d := make([]int, 10)
-	singleSlice(d)
-
+	sliceModify(a)
 	fmt.Printf("a: %v\n", a)
-	fmt.Printf("b: %v\n", b)
-	fmt.Printf("c: %v\n", c)
-	fmt.Printf("d: %v\n", d)
+
+	sliceAppend(a)
+	fmt.Printf("a: %v\n", a)
 }
 
-
-func slice(a []int) {
+func sliceModify(a []int) {
 	a[1] = 3
 }
-func singleSlice(a []int) {
-	 a= append(a, 1)
+
+func sliceAppend(a []int) {
+	a = append(a, 1)
 }
 
 func twoSlice(b [][]int) {
 	fmt.Printf("in before: %p\n", &b)
-	b = append(b, []int{1,2, 3})
+	b = append(b, []int{1, 2, 3})
 	fmt.Printf("in after: %p\n", &b)
 }
 func twoSlicePointer(b *[][]int) {
-	*b = append(*b, []int{1,2, 3})
+	*b = append(*b, []int{1, 2, 3})
 }
